6158605c232320bf4e49026b920a4f11348deedc
bimadevs <bimadev06@gmail.com>
2024-10-01 21:44:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PIL import Image
 import google.generativeai as genai
 
 # Configure the API key directly in the script
-API_KEY = 'AIzaSyDJdv5KwI8y0BIwbSir_poBvNbTVQemYDA'
+API_KEY = 'YOUR GEMINI API KEY'
 genai.configure(api_key=API_KEY)
 
 # Generation configuration
